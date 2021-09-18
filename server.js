c3c7761871ff466c1897db130efd32fcbdb21111
RihanFoudeh <Rihanfoudeh@gmail.com>
2021-09-18 11:46:14 +0300

    creat add Rout

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(express.json());
 
 
 server.get('/',Homehandler)
-
+server.post('/course', addCourse);
 
 
 
@@ -19,6 +19,43 @@ server.get('/',Homehandler)
 function Homehandler(res,req){
     res.send("HomePage");
 }
+
+
+
+async function addCourse(req, res) {
+    console.log(req.body);
+    const courseTitle = req.body.courseTitle;
+    const courseDescription = req.body.courseDescription;
+    const coursePrice = req.body.coursePrice;
+    const courseEmail = req.body.courseEmail;
+    // const {courseTitle, courseDescription, coursePrice,courseEmail} = req.body;
+    await Modelbook.create({
+        courseTitle: courseTitle,
+        courseDescription: courseDescription,
+        coursePrice: coursePrice,
+        courseEmail: courseEmail
+    });
+
+    // await KittenModel.create({catName,catBreed,ownerEmail});
+
+    ModelCourse.find({ ownerEmail: courseEmail }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
+
+}
+
+
+
+
+
+
+
+
 
 
 
